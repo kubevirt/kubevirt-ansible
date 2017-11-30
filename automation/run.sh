001f00b95528f434bcb3bb9a9f1e070771b3b72a
gbenhaim <galbh2@gmail.com>
2017-11-30 20:00:39 +0200

    Install openshift 3.7 rc
    Currently from Centos testing repo (until it will be released).
    
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- automation/run.sh
+++ automation/run.sh
@@ -92,7 +92,7 @@ main() {
 
     if [[ "$cluster_type" == "openshift" ]]; then
         [[ -e openshift-ansible ]] || \
-        git clone -b release-3.6 https://github.com/openshift/openshift-ansible
+        git clone -b release-3.7 https://github.com/openshift/openshift-ansible
         args+=(
             "openshift_ansible_dir=$(realpath openshift-ansible)"
             "cluster_type=openshift"
