f36b1f8d55a21928b0eaf2c4d52e3c463a4d8b05
gbenhaim <galbh2@gmail.com>
2018-02-21 17:14:56 +0200

    re-org: Move openshift deployment to playbooks dir
    - Custom variables per Openshift version can be declared under
      playbooks/cluster/openshift/vars/
    
    - Use a static version for Openshift 3.9 (v3.9.0-alpha.4)
    
    - Rename deploy-openshift.yml to config.yml.
    
    - Add the prerequisites of openshift to config.yml - there is no
      reason to keep them in a separate file.
    
    - Add a global variable file at vars/global_vars.yml
    
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- automation/check-patch.openshift_3-9.sh
+++ automation/check-patch.openshift_3-9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-export OPENSHIFT_IMAGE_TAG="v3.9.0"
+export OPENSHIFT_VER="3.9"
 export ANSIBLE_MODULES_VERSION="openshift-ansible-3.9.0-0.40.0"
 export OPENSHIFT_PLAYBOOK_PATH="playbooks/deploy_cluster.yml"
 "${0%/*}/check-patch.sh"
