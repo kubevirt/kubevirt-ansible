078ca38b144a212bb2324867c73161f498745206
gbenhaim <galbh2@gmail.com>
2018-06-03 21:16:19 +0300

    Allow to run check-patch without cleanup
    This option will make it very simple to run the automation
    locally and investigate the env.
    
    For deploying the env without cleanup:
    
    ./automation/check-patch.openshift_3-9.sh --skip-cleanup
    
    For removing the env:
    
    ./automation/check-patch.openshift_3-9.sh --only-cleanup
    
    The default is to cleanup the env.
    
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- automation/check-patch.openshift_3-9.sh
+++ automation/check-patch.openshift_3-9.sh
@@ -3,4 +3,4 @@
 export OPENSHIFT_VERSION="3.9"
 export ANSIBLE_MODULES_VERSION="openshift-ansible-3.9.0-0.40.0"
 export OPENSHIFT_PLAYBOOK_PATH="playbooks/deploy_cluster.yml"
-"${0%/*}/check-patch.sh"
+"${0%/*}/check-patch.sh" "$@"
