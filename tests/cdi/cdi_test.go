fd813daf4215c8fef84ec56a6c5dbe3fa1bd927d
Shiyang Wang <shiywang@redhat.com>
2018-07-24 15:43:08 +0800

    add negative tests and refactor util functions
    Signed-off-by: Shiyang Wang <shiywang@redhat.com>

--- tests/cdi/cdi_test.go
+++ tests/cdi/cdi_test.go
@@ -13,6 +13,7 @@ var _ = Describe("Importing and starting a VM using CDI", func() {
 	flag.Parse()
 	ktests.SkipIfNoOc()
 	dstPVCFilePath := "/tmp/test-pvc.json"
+	dstPVCFilePath1 := "/tmp/test-pvc1.json"
 	dstVMFilePath := "/tmp/test-vm.json"
 
 	url, ok := os.LookupEnv("STREAM_IMAGE_URL")
@@ -23,7 +24,7 @@ var _ = Describe("Importing and starting a VM using CDI", func() {
 	Context("PVC with valid image url will succeed", func() {
 		It("should create the PVC successfully", func() {
 			tests.ProcessTemplateWithParameters(rawPVCFilePath, dstPVCFilePath, "PVC_NAME="+pvcName, "EP_URL="+url)
-			tests.CreateResourceWithFilePathTestNamespace("pvc", pvcName, dstPVCFilePath)
+			tests.CreateResourceWithFilePathTestNamespace(dstPVCFilePath)
 		})
 
 		Specify("the PVC should become bound", func() {
@@ -31,16 +32,33 @@ var _ = Describe("Importing and starting a VM using CDI", func() {
 		})
 
 		Specify("the importer-pod should become completed", func() {
-			tests.WaitUntilResourceReadyByLabelTestNamespace("pod", "app=containerized-data-importer", "-o=jsonpath='{.items[0].status.phase}'", "Succeeded")
+			tests.WaitUntilResourceReadyByLabelTestNamespace("pod", tests.CDI_LABEL_SELECTOR, "-o=jsonpath='{.items[*].status.phase}'", "Succeeded")
+		})
+
+		It("should delete the importer-pod", func() {
+			tests.DeleteResourceWithLabelTestNamespace("pod", tests.CDI_LABEL_SELECTOR)
 		})
 
 		It("the VM should be created successfully", func() {
 			tests.ProcessTemplateWithParameters(rawVMFilePath, dstVMFilePath, "VM_NAME="+vmName, "PVC_NAME="+pvcName, "VM_APIVERSION="+vmAPIVersion)
-			tests.CreateResourceWithFilePathTestNamespace("vmi", vmName, dstVMFilePath)
+			tests.CreateResourceWithFilePathTestNamespace(dstVMFilePath)
 		})
 
 		Specify("the VM should be running", func() {
 			tests.WaitUntilResourceReadyByNameTestNamespace("vmi", vmName, "-o=jsonpath='{.status.phase}'", "Running")
 		})
 	})
+
+	Context("PVC with invalid image url will be failed", func() {
+		It("should create the PVC successfully", func() {
+			tests.ProcessTemplateWithParameters(rawPVCFilePath, dstPVCFilePath1, "PVC_NAME="+pvcName1, "EP_URL="+invalidPVCURL)
+			tests.CreateResourceWithFilePathTestNamespace(dstPVCFilePath1)
+		})
+
+		Specify("the PVC should become failed", func() {
+			tests.WaitUntilResourceReadyByLabelTestNamespace("pod", tests.CDI_LABEL_SELECTOR, "-o=jsonpath='{.items[*].status.phase}'", "Failed")
+		})
+	})
+
+
 })
