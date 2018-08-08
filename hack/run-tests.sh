f9ea1944379e474c212a15914455effbb89ca9a4
Lukas Bednar <lbednar@redhat.com>
2018-08-08 17:13:46 +0200

    run-tests.sh: add FUNC_TEST_ARGS param
    Add option to pass additional arguments to test execution,
    it will be useful on our environment when we want to generate junit
    results for example.
    
    Signed-off-by: Lukas Bednar <lbednar@redhat.com>

--- hack/run-tests.sh
+++ hack/run-tests.sh
@@ -8,5 +8,5 @@ prefix=kubevirt
 tag=v0.7.0
 kubeconfig=~/.kube/config
 
-${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -test.timeout 60m
-${TESTS_OUT_DIR}/cdi.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -test.timeout 60m
+${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -test.timeout 60m ${FUNC_TEST_ARGS}
+${TESTS_OUT_DIR}/cdi.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -test.timeout 60m ${FUNC_TEST_ARGS}
