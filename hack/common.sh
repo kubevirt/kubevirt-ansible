7e14e54b250b106d32698a3734946cc87e591ef6
Lukas Bednar <lbednar@redhat.com>
2018-08-14 14:58:27 +0200

    Generate junit report by default
    Signed-off-by: Lukas Bednar <lbednar@redhat.com>

--- hack/common.sh
+++ hack/common.sh
@@ -7,6 +7,8 @@ KUBEVIRT_ANSIBLE_DIR="$(
 OUT_DIR=$KUBEVIRT_ANSIBLE_DIR/_out
 TESTS_OUT_DIR=$OUT_DIR/tests
 
+FUNC_TEST_ARGS="${FUNC_TEST_ARGS:--test.timeout 60m --junit-output=junit.xml}"
+
 function build_func_tests() {
     mkdir -p ${TESTS_OUT_DIR}/
     ginkgo build ${KUBEVIRT_ANSIBLE_DIR}/tests
