2d7e621c6100ba1231f7cbf93538b4922b592d26
Lukas Bednar <lbednar@redhat.com>
2018-08-14 14:58:27 +0200

    Generate junit report by default
    Signed-off-by: Lukas Bednar <lbednar@redhat.com>

--- hack/run-tests.sh
+++ hack/run-tests.sh
@@ -4,9 +4,9 @@ set -e
 
 source hack/common.sh
 
-prefix=kubevirt
-tag=v0.7.0
-kubeconfig=~/.kube/config
+prefix=${DOCKER_PREFIX:-kubevirt}
+tag=${DOCKER_TAG:-v0.7.0}
+kubeconfig=${KUBECONFIG:-~/.kube/config}
 
-${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -test.timeout 60m ${FUNC_TEST_ARGS}
-${TESTS_OUT_DIR}/cdi.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -test.timeout 60m ${FUNC_TEST_ARGS}
+${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix ${FUNC_TEST_ARGS}
+${TESTS_OUT_DIR}/cdi.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix ${FUNC_TEST_ARGS}
--- tests/tests_suite_test.go
+++ tests/tests_suite_test.go
@@ -5,13 +5,21 @@ import (
 	. "github.com/onsi/gomega"
 
 	"testing"
-	
+
 	"kubevirt.io/kubevirt/tests"
+	"kubevirt.io/qe-tools/pkg/ginkgo-reporters"
 )
 
 func TestTests(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Tests Suite")
+	reporters := make([]Reporter, 0)
+	if ginkgo_reporters.Polarion.Run {
+		reporters = append(reporters, &ginkgo_reporters.Polarion)
+	}
+	if ginkgo_reporters.JunitOutput != "" {
+		reporters = append(reporters, ginkgo_reporters.NewJunitReporter())
+	}
+	RunSpecsWithDefaultAndCustomReporters(t, "Tests Suite", reporters)
 }
 
 var _ = BeforeSuite(func() {
