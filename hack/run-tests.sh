8d17650472989286415db2d3f29db8207ed854a2
gbenhaim <galbh2@gmail.com>
2018-08-28 18:25:21 +0300

    Test KubeVirt v0.8.0-alpha.1
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- hack/run-tests.sh
+++ hack/run-tests.sh
@@ -5,7 +5,7 @@ set -e
 source hack/common.sh
 
 prefix=${DOCKER_PREFIX:-kubevirt}
-tag=${DOCKER_TAG:-v0.7.0}
+tag=${DOCKER_TAG:-v0.8.0-alpha.1}
 kubeconfig=${KUBECONFIG:-~/.kube/config}
 
 ${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix ${FUNC_TEST_ARGS}
