aeacad410a41a39b1962aa825eb8b431af1fe189
Shiyang Wang <shiywang@redhat.com>
2018-08-31 15:32:32 +0800

    add oc kubectl virtctl path
    Signed-off-by: Shiyang Wang <shiywang@redhat.com>

--- hack/run-tests.sh
+++ hack/run-tests.sh
@@ -7,6 +7,9 @@ source hack/common.sh
 prefix=${DOCKER_PREFIX:-kubevirt}
 tag=${DOCKER_TAG:-v0.8.0-alpha.1}
 kubeconfig=${KUBECONFIG:-~/.kube/config}
+OC_PATH=$(command -v oc)
+KUBECTL_PATH=$(which kubectl)
+VIRTCTL_PATH=$(which virtctl)
 
-${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix ${FUNC_TEST_ARGS}
-${TESTS_OUT_DIR}/cdi.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix ${CDI_TEST_ARGS}
+${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -oc-path=${OC_PATH} -kubectl-path=${KUBECTL_PATH} -virtctl-path=${VIRTCTL_PATH} ${FUNC_TEST_ARGS}
+${TESTS_OUT_DIR}/cdi.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -oc-path=${OC_PATH} -kubectl-path=${KUBECTL_PATH} -virtctl-path=${VIRTCTL_PATH} ${CDI_TEST_ARGS}
