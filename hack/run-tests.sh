ad6949fef201714e279fdae07776d122a13a159c
Lukas Bednar <lbednar@redhat.com>
2018-09-18 19:00:11 +0200

    run-tests.sh: make paths to binaries configurable
    Signed-off-by: Lukas Bednar <lbednar@redhat.com>

--- hack/run-tests.sh
+++ hack/run-tests.sh
@@ -7,8 +7,8 @@ source hack/common.sh
 prefix=${DOCKER_PREFIX:-kubevirt}
 tag=${DOCKER_TAG:-v0.8.0-alpha.1}
 kubeconfig=${KUBECONFIG:-~/.kube/config}
-OC_PATH=$(command -v oc)
-KUBECTL_PATH=$(which kubectl)
-VIRTCTL_PATH=$(which virtctl)
+[ -z "$OC_PATH" ] && OC_PATH=$(command -v oc)
+[ -z "$KUBECTL_PATH" ] && KUBECTL_PATH=$(which kubectl)
+[ -z "$VIRTCTL_PATH" ] && VIRTCTL_PATH=$(which virtctl)
 
 ${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -oc-path=${OC_PATH} -kubectl-path=${KUBECTL_PATH} -virtctl-path=${VIRTCTL_PATH} ${FUNC_TEST_ARGS}
