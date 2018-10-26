991b9c8e6b5e8426d5c0c1eee1e52c5425d8b306
shiywang <shiywang@redhat.com>
2018-10-26 17:51:39 +0800

    add tmp fix

--- tests/e2e_test.go
+++ tests/e2e_test.go
@@ -42,8 +42,6 @@ var _ = Describe("Importing and starting a VM using CDI", func() {
 
 		It("will succeed", func() {
 			tests.WaitUntilResourceReadyByNameTestNamespace("pvc", pvcName, "-o=jsonpath='{.metadata.annotations}'", "pv.kubernetes.io/bind-completed:yes")
-			tests.WaitUntilResourceReadyByLabelTestNamespace("pod", tests.CDI_LABEL_SELECTOR, "-o=jsonpath='{.items[*].status.phase}'", "Succeeded")
-			tests.DeleteResourceWithLabelTestNamespace("pod", tests.CDI_LABEL_SELECTOR)
 			tests.ProcessTemplateWithParameters(rawVMFilePath, dstVMFilePath, "VM_NAME="+vmName, "PVC_NAME="+pvcName, "VM_APIVERSION="+vmAPIVersion)
 			tests.CreateResourceWithFilePathTestNamespace(dstVMFilePath)
 			tests.WaitUntilResourceReadyByNameTestNamespace("vmi", vmName, "-o=jsonpath='{.status.phase}'", "Running")
@@ -57,7 +55,7 @@ var _ = Describe("Importing and starting a VM using CDI", func() {
 		})
 
 		It("will be failed because the PVC should become failed", func() {
-			tests.WaitUntilResourceReadyByLabelTestNamespace("pod", tests.CDI_LABEL_SELECTOR, "-o=jsonpath='{.items[*].status.phase}'", "Failed")
+			tests.WaitUntilResourceReadyByLabelTestNamespace("pod", tests.CDI_LABEL_SELECTOR, "", "CrashLoopBackOff")
 		})
 	})
 
