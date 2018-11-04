c2e8e1450247c1cb2ad61fcd3f7f77b3301340de
gbenhaim <galbh2@gmail.com>
2018-11-04 19:29:51 +0200

    [WIP] Run tests in a constant order
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- hack/common.sh
+++ hack/common.sh
@@ -6,7 +6,7 @@ KUBEVIRT_ANSIBLE_DIR="$(
 )"
 OUT_DIR=$KUBEVIRT_ANSIBLE_DIR/_out
 TESTS_OUT_DIR=$OUT_DIR/tests
-FUNC_TEST_ARGS="${FUNC_TEST_ARGS:--test.timeout 60m --junit-output=exported-artifacts/tests.junit.xml}"
+FUNC_TEST_ARGS="${FUNC_TEST_ARGS:--test.timeout 60m --junit-output=exported-artifacts/tests.junit.xml -ginkgo.seed 1541336879}"
 
 function build_func_tests() {
     mkdir -p ${TESTS_OUT_DIR}/
