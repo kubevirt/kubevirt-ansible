a8feb428c14809663ef00775f4931edcf153cf91
gbenhaim <galbh2@gmail.com>
2018-11-04 19:29:51 +0200

    Run tests in a constant order
    We have sporadic failures on some of the tests. I suspect that some
    of the tests don't leave the cluster in a stable state (for example
    node_eviction test).
    
    In order to not block other PRs on CI, adding a temporary solution for
    running the tests in a constant order.
    
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- hack/common.sh
+++ hack/common.sh
@@ -7,7 +7,7 @@ KUBEVIRT_ANSIBLE_DIR="$(
 OUT_DIR=$KUBEVIRT_ANSIBLE_DIR/_out
 VENDOR_DIR=$KUBEVIRT_ANSIBLE_DIR/vendor
 TESTS_OUT_DIR=$OUT_DIR/tests
-FUNC_TEST_ARGS="${FUNC_TEST_ARGS:--test.timeout 60m --junit-output=exported-artifacts/tests.junit.xml}"
+FUNC_TEST_ARGS="${FUNC_TEST_ARGS:--test.timeout 60m --junit-output=exported-artifacts/tests.junit.xml -ginkgo.seed 1541336879}"
 
 function build_func_tests() {
     mkdir -p ${TESTS_OUT_DIR}/
