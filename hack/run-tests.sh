f839575f5e164b0fec85e42fb42d5706950b8722
Guohua Ouyang <gouyang@redhat.com>
2018-11-30 12:30:52 +0000

    cat kubeconfig

--- hack/run-tests.sh
+++ hack/run-tests.sh
@@ -19,6 +19,7 @@ oc get route -n kubevirt-web-ui
 set +e
 
 #${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -oc-path=${OC_PATH} -kubectl-path=${KUBECTL_PATH} -virtctl-path=${VIRTCTL_PATH} ${FUNC_TEST_ARGS}
+#ip=$(ifconfig bond0  | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 
 ${TESTS_OUT_DIR}/ui.test -webDriver=$webdriver
 
@@ -27,6 +28,8 @@ rpm -qa | grep chrom
 yum install chromium net-tools -y
 oc get route -n kubevirt-web-ui
 ping kubevirt-web-ui.cloudapps.example.com
+cat /etc/hosts
+cat .kube/config
 ifconfig
 
 find . | grep png
