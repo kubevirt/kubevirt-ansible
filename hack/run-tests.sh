901628d0463113304739174841bdcfa01a39dba3
Guohua Ouyang <gouyang@redhat.com>
2018-11-30 08:11:56 +0000

    Skip tests.test

--- hack/run-tests.sh
+++ hack/run-tests.sh
@@ -18,16 +18,15 @@ oc get route -n kubevirt-web-ui
 # turn off exit on error, so below test can be run.
 set +e
 
-${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -oc-path=${OC_PATH} -kubectl-path=${KUBECTL_PATH} -virtctl-path=${VIRTCTL_PATH} ${FUNC_TEST_ARGS}
+#${TESTS_OUT_DIR}/tests.test -kubeconfig=$kubeconfig -tag=$tag -prefix=$prefix -oc-path=${OC_PATH} -kubectl-path=${KUBECTL_PATH} -virtctl-path=${VIRTCTL_PATH} ${FUNC_TEST_ARGS}
 
 ${TESTS_OUT_DIR}/ui.test -webDriver=$webdriver
 
 rpm -qa | grep chrom
 
-yum install chromium -y
+yum install chromium net-tools -y
 oc get route -n kubevirt-web-ui
 ping kubevirt-web-ui.cloudapps.example.com
-nslookup kubevirt-web-ui.cloudapps.example.com
 ifconfig
 
 find . | grep png
