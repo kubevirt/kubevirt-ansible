19873dba70e5758f0858ee3e55feb4ea3662e638
gbenhaim <galbh2@gmail.com>
2018-12-27 13:50:00 +0200

    3.11: Update openshift-ansible modules
    Also, removed the "openshift_node_labels" since the new version of the
    modules requires that this variable will not be defined.
    
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- automation/check-patch.openshift_3-11.sh
+++ automation/check-patch.openshift_3-11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 export OPENSHIFT_VERSION="3.11"
-export ANSIBLE_MODULES_VERSION="openshift-ansible-3.11.43-1-gluster-fix"
+export ANSIBLE_MODULES_VERSION="openshift-ansible-3.11.86-1"
 export OPENSHIFT_PLAYBOOK_PATH="playbooks/deploy_cluster.yml"
-export OPENSHIFT_ANSIBLE_URL="https://github.com/gbenhaim/openshift-ansible.git"
+export OPENSHIFT_ANSIBLE_URL="https://github.com/openshift/openshift-ansible.git"
 "${0%/*}/check-patch.sh" "$@"
