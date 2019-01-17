7198511c131723769d1eead7d709fcb6ffbe88c1
Shiyang Wang <shiywang@redhat.com>
2019-01-17 20:52:16 +0800

    remove namespace duplication (#505)

--- tests/framework/util.go
+++ tests/framework/util.go
@@ -27,22 +27,22 @@ func ProcessTemplateWithParameters(srcFilePath, dstFilePath string, params ...st
 
 func CreateResourceWithFilePathTestNamespace(filePath string) {
 	By("Creating resource from the json file with the oc-create command")
-	execute(Result{cmd: "oc", verb: "create", filePath: filePath, nameSpace: NamespaceTestDefault})
+	execute(Result{cmd: "oc", verb: "create", filePath: filePath})
 }
 
 func DeleteResourceWithLabelTestNamespace(resourceType, resourceLabel string) {
 	By(fmt.Sprintf("Deleting %s:%s from the json file with the oc-delete command", resourceType, resourceLabel))
-	execute(Result{cmd: "oc", verb: "delete", resourceType: resourceType, resourceLabel: resourceLabel, nameSpace: NamespaceTestDefault})
+	execute(Result{cmd: "oc", verb: "delete", resourceType: resourceType, resourceLabel: resourceLabel})
 }
 
 func WaitUntilResourceReadyByNameTestNamespace(resourceType, resourceName, query, expectOut string) {
 	By(fmt.Sprintf("Wait until %s with name %s ready", resourceType, resourceName))
-	execute(Result{cmd: "oc", verb: "get", resourceType: resourceType, resourceName: resourceName, query: query, expectOut: expectOut, nameSpace: NamespaceTestDefault})
+	execute(Result{cmd: "oc", verb: "get", resourceType: resourceType, resourceName: resourceName, query: query, expectOut: expectOut})
 }
 
 func WaitUntilResourceReadyByLabelTestNamespace(resourceType, label, query, expectOut string) {
 	By(fmt.Sprintf("Wait until resource %s with label=%s ready", resourceType, label))
-	execute(Result{cmd: "oc", verb: "get", resourceType: resourceType, resourceLabel: label, query: query, expectOut: expectOut, nameSpace: NamespaceTestDefault})
+	execute(Result{cmd: "oc", verb: "get", resourceType: resourceType, resourceLabel: label, query: query, expectOut: expectOut})
 }
 
 func writeJson(jsonFile string, json string) (string, error) {
@@ -55,7 +55,7 @@ func writeJson(jsonFile string, json string) (string, error) {
 
 func RunOcDescribeCommand(resourceType, resourceName string) string {
 	fmt.Printf("Getting 'oc describe' with: %s ", resourceName)
-	return execute(Result{cmd: "oc", verb: "describe", resourceType: resourceType, resourceName: resourceName, nameSpace: NamespaceTestDefault})
+	return execute(Result{cmd: "oc", verb: "describe", resourceType: resourceType, resourceName: resourceName})
 }
 
 // generatePrivateKey creates a RSA Private Key of specified byte size
